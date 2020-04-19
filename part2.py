0b4a76a1c033061b3ffada5a0532670be049cffc
EdanZwick <edan.zwick@gmail.com>
2020-04-19 22:39:50 +0300

    Question2- part1

--- part2.py
+++ part2.py
@@ -1,5 +1,8 @@
 import torch
 import torchvision
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class Flatten:
@@ -52,24 +55,41 @@ class Net:
         p = []
         for layer in self.layers:
             p.append(layer.params())
+        p.append(self.output.params())
         return p
 
 
-def grid_search(train_loader, validation_loader, learning_rates, momentum, in_dim, out_dim, hidden_dim):
+def optimizer_grid_search(train_loader, validation_loader, learning_rates, momentum, in_dim, out_dim, hidden_dim, std):
     heat_map = [[0 for i in learning_rates] for j in momentum]
     best_loss = 10000000
     for i, lr in enumerate(learning_rates):
         for j, m in enumerate(momentum):
-            net = Net(in_dim, out_dim, hidden_dim, std=1)
+            net = Net(in_dim, out_dim, hidden_dim, std=std)
             optimizer = torch.optim.SGD(net.params(), lr=lr, momentum=m)
             train(net, train_loader, optimizer)
-            validation_loss = run(net, validation_loader)
-            print('For parameters: lr-{}, momentum-{} got loss: {}'.format(lr, m, validation_loss))
+            validation_loss, acc = run(net, validation_loader)
+            print('For parameters: lr- {}, momentum- {}, got loss: {}, acc- {}'.format(lr, m, validation_loss, acc))
             if validation_loss < best_loss:
                 best_loss = validation_loss
                 best_params = {'lr': lr, 'momentum': m}
             heat_map[j][i] = validation_loss
     return best_params, best_loss, heat_map
+
+
+def weights_grid_search(train_loader, validation_loader, deviations, in_dim, out_dim, hidden_dim, lr , m):
+    heat_map = [0 for j in deviations]
+    best_loss = 10000000
+    for i, std in enumerate(deviations):
+        net = Net(in_dim, out_dim, hidden_dim, std=std)
+        optimizer = torch.optim.SGD(net.params(), lr=lr, momentum=m)
+        train(net, train_loader, optimizer)
+        validation_loss, acc = run(net, validation_loader)
+        print('For parameter: std- {} got loss: {}, acc: {}'.format(std, validation_loss, acc))
+        if validation_loss < best_loss:
+            best_loss = validation_loss
+            best_param = std
+        heat_map[i] = validation_loss
+    return best_param, best_loss, heat_map
 
 
 def train(net, loader, optimizer, verbose=False, num_epochs=100):
@@ -85,16 +105,25 @@ def train(net, loader, optimizer, verbose=False, num_epochs=100):
             optimizer.step()
             epoch_loss += loss * batch.shape[0]  # avg. weighted per sample, as not all batches are equal
         if verbose:
-            print('Epoch: {}, loss {}'.format(epoch, epoch_loss/len(loader)))
-        if verbose and epoch % 10 == 0:
-            print('epoch: {}'.format(epoch))
+            print('Epoch: {}, loss {}'.format(epoch, epoch_loss/len(loader.dataset)))
 
 
 def run(net, loader):
+    acc = 0
     loss = 0
     for i, (batch, target) in enumerate(loader):
         batch = batch.cuda() if torch.cuda.is_available() else batch
         target= target.cuda() if torch.cuda.is_available() else target
         output = net.forward(batch)
-        loss += torch.nn.functional.binary_cross_entropy_with_logits(output, target) * batch.shape[0]
-    return loss / len(loader)
+        loss += torch.nn.functional.binary_cross_entropy_with_logits(output, target).item() * batch.shape[0]
+        acc += (output.argmax(dim=1)==target.argmax(dim=1)).sum().item()
+    return (loss / len(loader.dataset)), (acc / len(loader.dataset))
+
+
+# Pretty-prints grid search matrix (taken off S.O.)
+def show_heat_map(table, lr, m):
+    df_table = pd.DataFrame(table, m, lr)
+    # plt.figure(figsize=(10,7))
+    sn.set(font_scale=1.4)  # for label size
+    sn.heatmap(df_table, annot=True, annot_kws={"size": 16})  # font size
+    plt.show()

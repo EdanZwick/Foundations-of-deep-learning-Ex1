24f130bd14f26a6871da462855e000e7126930ee
EdanZwick <edan.zwick@gmail.com>
2020-04-29 10:52:25 +0300

    Finished Adam section.

--- part2.py
+++ part2.py
@@ -1,8 +1,10 @@
 import torch
 import torchvision
+import numpy as np
 import seaborn as sn
 import pandas as pd
 import matplotlib.pyplot as plt
+
 
 class Flatten:
     def __call__(self, sample):
@@ -21,8 +23,12 @@ class OneHot:
 
 
 class Linear:
-    def __init__(self, D_in, D_out, std):
-        self.w = torch.empty((D_in + 1, D_out), dtype=torch.float).normal_(mean=0, std=std)
+    def __init__(self, D_in, D_out, std=0.02, xavier=False):
+        if xavier:
+            u = np.sqrt(6 / (D_in + D_out))
+            self.w = torch.empty((D_in + 1, D_out), dtype=torch.float).uniform_(-std, std)
+        else:
+            self.w = torch.empty((D_in + 1, D_out), dtype=torch.float).normal_(mean=0, std=std)
         if torch.cuda.is_available():
             self.w = self.w.cuda()
         self.w.requires_grad = True
@@ -36,17 +42,34 @@ class Linear:
         return self.w
 
 
-class Net:
-    def __init__(self, din, dout, dhidden, std):
-        self.layers = []
-        for dim in dhidden:
-            self.layers.append(Linear(din, dim, std))
-            din = dim
-        self.output = Linear(din, dout, std)  # output  layer
+#  P is the probability for a neuron to be droped out.
+#  drop out is done by multiplying with a binary mask.
+#  train time results are devided by p so value's expectaition will be the same during test time (where no drop-out is done).
+class Dropout:
+    def __init__(self, p, train):
+        self.p = 1 - p
+        self.train = train
 
     def forward(self, x):
+        if self.train:
+            mask = torch.bernoulli(self.p * torch.ones_like(x))
+            x = x * mask / self.p  # all multipicaion and division is done element wise
+        return x
+
+
+class Net:
+    def __init__(self, din, dout, dhidden, std=0.02, xavier=False, dropout_p=0, train=False):
+        self.layers = []
+        for dim in dhidden:
+            self.layers.append(Linear(din, dim, std, xavier))
+            din = dim
+        self.output = Linear(din, dout, std)  # output  layer
+        self.dropout_p = dropout_p
+
+    def forward(self, x):
+        self.dropout = Dropout(self.dropout_p, train) if (train and self.dropout_p > 0) else None
         for layer in self.layers:
-            x = layer.forward(x)
+            x = layer.forward(x) if self.dropout is None else self.dropout.forward(layer.forward(x))
             torch.nn.functional.relu(x, inplace=True)
         return self.output.forward(x)
 
@@ -77,6 +100,7 @@ def optimizer_grid_search(train_loader, validation_loader, learning_rates, momen
             if verbose:
                 print('For parameters: lr- {}, momentum- {}, got loss: {}, acc- {}'.format(lr, m, validation_loss, acc))
     return best_params, best_loss, loss_heat_map, acc_heat_map
+
 
 def adam_grid_search(train_loader, validation_loader, learning_rates, betas, in_dim, out_dim, hidden_dim,
                      std, num_epochs=75, verbose=False):
@@ -118,7 +142,7 @@ def weights_grid_search(train_loader, validation_loader, deviations, in_dim, out
 
 
 def adam_weights_grid_search(train_loader, validation_loader, deviations, in_dim, out_dim, hidden_dim,
-                        lr, betas, num_epochs=75, verbose=False):
+                             lr, betas, num_epochs=75, verbose=False):
     heat_map = [0 for j in deviations]
     best_loss = 10000000
     for i, std in enumerate(deviations):
@@ -134,7 +158,63 @@ def adam_weights_grid_search(train_loader, validation_loader, deviations, in_dim
             print('For parameter: std- {} got loss: {}, acc: {}'.format(std, validation_loss, acc))
     return best_param, best_loss, heat_map
 
+
+def adam_decay_search(train_loader, validation_loader, decays, in_dim, out_dim, hidden_dim,
+                      lr, betas, std, num_epochs=75, verbose=False):
+    heat_map = [0 for j in decays]
+    best_loss = 10000000
+    for i, d in enumerate(decays):
+        net = Net(in_dim, out_dim, hidden_dim, std=std)
+        optimizer = torch.optim.Adam(net.params(), lr=lr, betas=betas, weight_decay=d)
+        train(net, train_loader, optimizer, num_epochs=num_epochs)
+        validation_loss, acc = run(net, validation_loader)
+        if validation_loss < best_loss:
+            best_loss = validation_loss
+            best_param = std
+        heat_map[i] = acc
+        if verbose:
+            print('For parameter: std- {} got loss: {}, acc: {}'.format(std, validation_loss, acc))
+    return best_param, best_loss, heat_map
+
+
+def sgd_decay_search(train_loader, validation_loader, decays, in_dim, out_dim, hidden_dim, lr, m, std, num_epochs=75,
+                     verbose=False):
+    heat_map = [0 for j in decays]
+    best_loss = 10000000
+    for i, d in enumerate(decays):
+        net = Net(in_dim, out_dim, hidden_dim, std=std)
+        optimizer = torch.optim.SGD(net.params(), lr=lr, momentum=m, weight_decay=d)
+        train(net, train_loader, optimizer, num_epochs=num_epochs)
+        validation_loss, acc = run(net, validation_loader)
+        if validation_loss < best_loss:
+            best_loss = validation_loss
+            best_param = std
+        heat_map[i] = acc
+        if verbose:
+            print('For parameter: std- {} got loss: {}, acc: {}'.format(std, validation_loss, acc))
+    return best_param, best_loss, heat_map
+
+
+def adam_dropout_search(train_loader, validation_loader, dropout_pobs, in_dim, out_dim, hidden_dim,
+                        lr=0.0005, betas=(0.9, 0.999), std=0.02, num_epochs=75, verbose=False):
+    heat_map = [0 for j in dropout_pobs]
+    best_loss = 10000000
+    for i, p in enumerate(dropout_pobs):
+        net = Net(in_dim, out_dim, hidden_dim, std=std, dropout_p=p)
+        optimizer = torch.optim.Adam(net.params(), lr=lr, betas=betas)
+        train(net, train_loader, optimizer, num_epochs=num_epochs)
+        validation_loss, acc = run(net, validation_loader)
+        if validation_loss < best_loss:
+            best_loss = validation_loss
+            best_param = std
+        heat_map[i] = acc
+        if verbose:
+            print('For parameter: std- {} got loss: {}, acc: {}'.format(std, validation_loss, acc))
+    return best_param, best_loss, heat_map
+
+
 def train(net, loader, optimizer, test_set=None, verbose=False, num_epochs=75):
+    net.train = True
     train_losses = []
     train_accs = []
     test_losses = []
@@ -162,34 +242,38 @@ def train(net, loader, optimizer, test_set=None, verbose=False, num_epochs=75):
             test_losses.append(test_loss)
             test_accs.append(test_acc)
             if verbose:
-                print('Epoch: {}, loss: {}, acc: {}, test loss: {}, test acc: {}'.format(epoch, train_loss, train_acc, test_loss, test_acc))
+                print('Epoch: {}, loss: {}, acc: {}, test loss: {}, test acc: {}'.format(epoch, train_loss, train_acc,
+                                                                                         test_loss, test_acc))
         elif verbose:
-            print('Epoch: {}, loss: {}, acc: {}, test loss: {}, test acc: {}'.format(epoch, train_loss, train_acc, test_loss, test_acc))
+            print('Epoch: {}, loss: {}, acc: {}, test loss: {}, test acc: {}'.format(epoch, train_loss, train_acc,
+                                                                                     test_loss, test_acc))
+    net.train = False
     return train_losses, train_accs, test_losses, test_accs
 
 
 def run(net, loader):
+    net.train = False
     acc = 0
     loss = 0
     for i, (batch, target) in enumerate(loader):
         batch = batch.cuda() if torch.cuda.is_available() else batch
-        target= target.cuda() if torch.cuda.is_available() else target
+        target = target.cuda() if torch.cuda.is_available() else target
         output = net.forward(batch)
         loss += torch.nn.functional.binary_cross_entropy_with_logits(output, target).item() * batch.shape[0]
-        acc += (output.argmax(dim=1)==target.argmax(dim=1)).sum().item()
+        acc += (output.argmax(dim=1) == target.argmax(dim=1)).sum().item()
     return (loss / len(loader.dataset)), (acc / len(loader.dataset))
 
 
 # Pretty-prints grid search matrix (taken off S.O.)
 def show_heat_map(table, lr, m, xlabel='', ylabel='', invert=False):
-    lr = [round(num,4) for num in lr]
+    lr = [round(num, 4) for num in lr]
     if type(m[0]) is tuple:
-        m = [tuple([round(x,4) for x in num]) for num in m]
+        m = [tuple([round(x, 4) for x in num]) for num in m]
     else:
-        m = [round(num,4) for num in m]
-    #table = [[round(num, 4) for num in row] for row in table]
+        m = [round(num, 4) for num in m]
+    # table = [[round(num, 4) for num in row] for row in table]
     df_table = pd.DataFrame(table, m, lr)
-    plt.figure(figsize=(15,10))
+    plt.figure(figsize=(15, 10))
     sn.set(font_scale=1.4)  # for label size
     if invert:
         sn.heatmap(df_table, annot=True, annot_kws={"size": 12}, cmap=sn.cm.rocket_r, fmt='.3g')  # font size
@@ -198,11 +282,11 @@ def show_heat_map(table, lr, m, xlabel='', ylabel='', invert=False):
     plt.show()
 
 
-def plot_train_test(train_results, test_results, epochs, label_name, cat1 = 'train ', cat2 = 'test '):
+def plot_train_test(train_results, test_results, epochs, label_name, cat1='train ', cat2='test '):
     ticks = [i for i, _ in enumerate(train_results)]
     plt.figure(figsize=(15, 10))
     plt.plot(ticks, train_results, '-b', label=(cat1 + label_name))
-    plt.plot(ticks, test_results, '-r', label= (cat2 + label_name))
+    plt.plot(ticks, test_results, '-r', label=(cat2 + label_name))
     plt.legend()
     plt.title('Test and train ' + label_name + ' per epoch')
     plt.show()
